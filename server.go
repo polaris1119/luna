9adda4e690d93498abe5715d3b3485742ad88394
polaris <polaris@studygolang.com>
2016-01-05 17:06:24 +0800

    rpc

--- server.go
+++ server.go
@@ -1,3 +1,9 @@
+// Copyright 2016 polaris. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+// http://studygolang.com
+// Author：polaris	polaris@studygolang.com
+
 package luna
 
 import (
